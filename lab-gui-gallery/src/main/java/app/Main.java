e8fad353ec8b7b0100bdbbf844b60a262ed9e0b2
Julija <juleczek2002@gmail.com>
2023-11-10 16:43:44 +0100

    Javafx done

---
+++ lab-gui-gallery/src/main/java/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static final String SCRAPER_API_KEY = "***778dfc2956b8cf3a2023235531245500 ***";
+
+
+    public static void main(String[] args) {
+        Application.launch(GalleryApp.class);
+    }
+}
